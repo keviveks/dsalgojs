0028749e98204790af4fe333d48f85c06528f056
keviveks <keviveks@gmail.com>
2020-04-01 12:59:48 +0530

    Stack

---
+++ stack/example.js
@@ -0,0 +1,31 @@
+/* Stack */
+
+/*
+Methodoligy: LIFO
+Functions:
+ - stack.push
+ - stack.pop
+ - stack.peek
+ - stack.length
+Feature in JS: Array
+*/
+
+var letters = []; // this will be our stack
+
+var word = 'kevivek';
+
+var rword = '';
+
+for (var i = 0; i < word.length; i++) {
+  letters.push(word[i]);
+}
+
+for (var i = 0; i < word.length; i++) {
+  rword += letters.pop();
+}
+
+if (word === rword) {
+  console.log(word + ' is a palindrome.');
+} else {
+  console.log(word + ' is not a palindrome.');
+}
---
+++ stack/stack.js
@@ -0,0 +1,55 @@
+/* Stack */
+
+/*
+Methodoligy: LIFO
+Functions:
+ - stack.push
+ - stack.pop
+ - stack.peek
+ - stack.length/stack.size
+Feature in JS: Array
+*/
+
+var Stack = function() {
+  this.count = 0;
+  this.storage = {};
+
+  // Adds a vlaue onto the end of the stack
+  this.push = function (value) {
+    this.storage[this.count] = value;
+    this.count++;
+  }
+
+  // Removes and returns the value at the end of the stack
+  this.pop = function() {
+    if (this.count === 0) {
+      return undefined;
+    }
+    this.count--;
+    var result = this.storage[this.count];
+    delete this.storage[this.count];
+    return result;
+  }
+
+  // Returns the value at the end of the stack
+  this.peek = function() {
+    return this.storage[this.count-1];
+  }
+
+  this.size = function() {
+    return this.count;
+  }
+}
+
+var myStack = new Stack();
+
+myStack.push(1);
+myStack.push(2);
+console.log(myStack.peek());
+console.log(myStack.pop());
+console.log(myStack.peek());
+myStack.push('keviveks');
+console.log(myStack.size());
+console.log(myStack.peek());
+console.log(myStack.pop());
+console.log(myStack.peek());
