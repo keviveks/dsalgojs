0d31e989fcdda8e28ccc609d6372bfd9be17233b
keviveks <keviveks@gmail.com>
2020-04-13 12:28:23 +0530

    Trie data structure complete

--- trie/trie.js
+++ trie/trie.js
@@ -26,8 +26,53 @@ let Trie = function() {
     } else {
       return this.add(input.substr(1), node.keys.get(input[0]));
     }
-  }
+  };
+
+  this.isWord = function(word) {
+    let node = this.root;
+    while(word.length > 1) {
+      if (!node.keys.has(word[0])) {
+        return false;
+      } else {
+        node = node.keys.get(word[0]);
+        word = word.substr(1);
+      }
+    }
+    return (node.keys.has(word) && node.keys.get(word).isEnd()) ? true : false;
+  };
+
+  this.print = function() {
+    let words = new Array();
+    let search = function(node, string) {
+      if (node.keys.size != 0) {
+        for (let letter of node.keys.keys()) {
+          search(node.keys.get(letter), string.concat(letter));
+        }
+        if (node.isEnd()) {
+          words.push(string);
+        }
+      } else {
+        (string.length > 0) ? words.push(string) : undefined;
+        return;
+      }
+    };
+    search(this.root, new String());
+    return (words.length > 0) ? words : null;
+  };
+
 };
 
 const myTrie = new Trie();
-
+myTrie.add('ball');
+myTrie.add('bat');
+myTrie.add('doll');
+myTrie.add('dark');
+myTrie.add('do');
+myTrie.add('dorm');
+myTrie.add('sent');
+myTrie.add('sense');
+console.log(myTrie.isWord('dorm'));
+console.log(myTrie.isWord('dor'));
+console.log(myTrie.isWord('dork'));
+console.log(myTrie.isWord('sent'));
+console.log(myTrie.print());
